f11d395db8a09eabb60b9b3628e74242bf2e8f21
Oritana <orio.chabo@gmail.com>
2022-07-01 12:17:44 +0900

    p195パラメータ結合の利用

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -12,10 +12,17 @@ use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
     public function index(Request $request)
-    {
-        $items = DB::select('select * from people');
-        return view('hello.index', ['items' => $items]);
-     }
+{
+   if (isset($request->id))
+   {
+      $param = ['id' => $request->id];
+      $items = DB::select('select * from people where id = :id',
+         $param);
+   } else {
+      $items = DB::select('select * from people');
+   }
+   return view('hello.index', ['items' => $items]);
+    }
     public function post(HelloRequest $request)
    {
        return view('hello.index', ['msg'=>'正しく入力されました！']);
