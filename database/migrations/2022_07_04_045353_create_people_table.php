58efbeadb71bbbe281a8ab393d7f6ec08f8aea28
Oritana <orio.chabo@gmail.com>
2022-07-04 19:10:42 +0900

    p233 マイグレーションを試す～シーディングを実行するまで

--- database/migrations/2022_07_04_045353_create_people_table.php
+++ database/migrations/2022_07_04_045353_create_people_table.php
@@ -3,22 +3,24 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\QueryException;
 
 class CreatePeopleTable extends Migration
 {
     public function up()
-    {
-       Schema::create('people', function (Blueprint $table) {
-           $table->increments('id');
-           $table->string('name');
-           $table->string('mail');
-           $table->integer('age');
-           $table->timestamps();
-       });
-    }
+{
+   Schema::create('people', function (Blueprint $table) {
+       $table->increments('id');
+       $table->string('name');
+       $table->string('mail');
+       $table->integer('age');
+       $table->timestamps();
+   });
+}
 
-   public function down()
-   {
-       //
-   }
+public function down()
+{
+   Schema::dropIfExists('people');
+}
+
 }
