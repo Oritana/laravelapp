c3e589b438c235daadc4591956c0f083dcf5567c
Oritana <orio.chabo@gmail.com>
2022-07-04 12:34:00 +0900

    p217 insertによるレコード追加

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -12,10 +12,12 @@ use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
     public function index(Request $request)
-    {
-       $items = DB::table('people')->get();
-       return view('hello.index', ['items' => $items]);
-    }
+{
+   $items = DB::table('people')->orderBy('age', 'asc')->get();
+   return view('hello.index', ['items' => $items]);
+}
+
+
     
 
    public function post(Request $request)
@@ -25,20 +27,20 @@ class HelloController extends Controller
    }
 
    public function add(Request $request)
-   {
-       return view('hello.add');
-   }
+{
+   return view('hello.add');
+}
 
-   public function create(Request $request)
-   {
-       $param = [
-           'name' => $request->name,
-           'mail' => $request->mail,
-           'age' => $request->age,
-       ];
-       DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
-       return redirect('/hello');
-   }
+public function create(Request $request)
+{
+   $param = [
+       'name' => $request->name,
+       'mail' => $request->mail,
+       'age' => $request->age,
+   ];
+   DB::table('people')->insert($param);
+   return redirect('/hello');
+}
 
    public function edit(Request $request)
     {
@@ -74,12 +76,13 @@ public function update(Request $request)
    }   
 
    public function show(Request $request)
-{
-   $min = $request->min;
-   $max = $request->max;
+    {
+   $page = $request->page;
    $items = DB::table('people')
-       ->whereRaw('age >= ? and age <= ?',
-        [$min, $max])->get();
+       ->offset($page * 3)
+       ->limit(3)
+       ->get();
    return view('hello.show', ['items' => $items]);
-}
+    }
+
 }
