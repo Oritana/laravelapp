83d0196e3e39126b75a44496af0be65cfc5963c6
Oritana <orio.chabo@gmail.com>
2022-07-10 15:44:54 +0900

    実習5-2

--- resources/views/jisyu5_2/del.blade.php
+++ resources/views/jisyu5_2/del.blade.php
@@ -11,10 +11,10 @@
    <table>
     @foreach ($items as $item)                       {{-- @foreachで $items から順にオブジェクトを $item に取り出していく --}}
       @csrf                                                   {{-- 'hidden'隠しフィールドで主キー(id)も送信 --}}
-      <input type="hidden" name="id" value="{{$item->id}}">   {{-- valueに $items から取り出した $item の値を設定(表示) --}}
-      <tr><th>name: </th><td>{{$item->name}}</td></tr>        {{-- $item に取り出した値を name　に表示し入力上書き --}}
-      <tr><th>mail: </th><td>{{$item->mail}}</td></tr>        {{-- $item に取り出した値を mail　に表示し入力上書き --}}
-      <tr><th>age: </th><td>{{$item->age}}</td></tr>          {{-- $item に取り出した値を age　に表示し入力上書き --}}
+      <input type="hidden" name="id" value="{{$item->id}}">   {{-- $items から取り出した $item で'id'にアクセスしvalueに設定 --}}
+      <tr><th>name: </th><td>{{$item->name}}</td></tr>        {{-- $item に取り出した'id'のレコードの name　を表示 --}}
+      <tr><th>mail: </th><td>{{$item->mail}}</td></tr>        {{-- $item に取り出した'id'のレコードの mail を表示 --}}
+      <tr><th>age: </th><td>{{$item->age}}</td></tr>          {{-- $item に取り出した'id'のレコードの age を表示 --}}
       <tr><th></th><td><input type="submit" value="削除"></td></tr> {{-- 削除をクリックするとデータが削除される 　　↑　 --}}
       @endforeach
 
